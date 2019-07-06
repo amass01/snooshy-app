0bfbba774d0b8f619e263fe3ea18416d07b68dde
Amir Massarwa <amassarwi@gmail.com>
2019-07-06 23:54:35 +0200

    Added temp content to home component & added navigation menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
-import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
+    { path: "", redirectTo: "/home", pathMatch: "full" },
+    { path: "home", component: HomeComponent },
     { path: "item/:id", component: ItemDetailComponent }
 ];
 
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { Page } from 'tns-core-modules/ui/page/page';
+import { Item } from '../data/item.model';
+import { Category } from '../data/category.model';
+import { DataService } from '../data/data.service';
+
+@Component({
+  selector: 'ns-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  lastDelY = 0;
+    headerCollapsed = false;
+    selectedTab = 0;
+    selectedTabview = 0;
+    items: Array<Item>;
+    categories: Array<Category>;
+
+  constructor(
+    private page: Page,
+    private dataService: DataService,
+  ) { }
+
+  ngOnInit(): void {
+    this.items = this.dataService.getItems();
+    this.categories = this.dataService.getCategories();
+    this.page.actionBarHidden = true;
+  }
+
+  showItem(itemId: string) {
+    console.log(`Tapped on ${itemId}`);
+    // this.routerExtensions.navigate(["detail/" + itemId, {
+    //     animated: true,
+    //     transition: {
+    //         name: "slideTop",
+    //         duration: 380,
+    //         curve: "easeIn"
+    //     }
+    // }]);
+}
+
+toggleLike(item) {
+    item.isLike = !item.isLike;
+    if (item.isLike) {
+        item.likes += 1;
+    } else {
+        item.likes -= 1;
+    }
+}
+
+toggleHeart(item) {
+    item.isFavorite = !item.isFavorite;
+}
+
+categoryIcon(itemCategory: string) {
+    switch (itemCategory) {
+        case "Burger":
+            return String.fromCharCode(0xf0f5); //"fa-cutlery";
+            break;
+        case "Beer":
+            return String.fromCharCode(0xf0fc); //"fa-beer";
+            break;
+        case "Pancake":
+            return String.fromCharCode(0xf0f4); //"fa-coffee";
+            break;
+        case "Cake":
+            return String.fromCharCode(0xf1fd); //"fa-birthday-cake";
+            break;
+        default:
+            return String.fromCharCode(0xf06d); //"fa-fire";
+            break;
+    }
+}
+
+  onPopularTap() {
+    this.selectedTabview = 0;
+  }
+
+  onCategoryTap() {
+      this.selectedTabview = 1;
+  }
+
+  onPromosTap() {
+      this.selectedTabview = 2;
+  }
+}
